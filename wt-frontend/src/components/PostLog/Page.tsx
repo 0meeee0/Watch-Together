7cbf3959ddd8a151c4380e4015ca7db82f870b54
0meeee0 <mehdi74.id@gmail.com>
2024-11-23 17:16:59 +0100

    Feat: username displays after logging in

--- wt-frontend/src/components/PostLog/Page.tsx
+++ wt-frontend/src/components/PostLog/Page.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import Nav from "../Nav/Nav";
 
 export default function Page() {
+    const name = localStorage.getItem('name')
   return (
     <header className="border-b border-white/10 p-4">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
-        <h1 className="text-xl font-bold">Video Room App</h1>
+        <h1 className="text-xl font-bold text-white">Welcome {name}</h1>
         <button className="flex items-center border border-white text-white hover:bg-white/10 px-8 py-3 rounded-lg">
           <PlusCircleIcon className="mr-2 h-5 w-5" />
           Create Room
